d0f2b6a9439f48f9c885727a16fad678f4cc6f22
evanezcent <pratamayo16@gmail.com>
2021-04-11 20:04:52 +0700

    finished

--- LoginApp/ContentView.swift
+++ LoginApp/ContentView.swift
@@ -22,6 +22,7 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct Home: View {
+    @State var index = 0
     
     var body: some View{
         GeometryReader {geo in
@@ -31,9 +32,50 @@ struct Home: View {
                     .resizable()
                     .frame(width: 60, height: 60)
                 
-                // Box login
-                RegisterFormView()
+                // Box login register
+                ZStack {
+                    
+                    RegisterFormView(index: self.$index)
+                        .zIndex(Double(self.index))
+                    LoginFormView(index: self.$index)
+                }
+                
+                // Divider
+                HStack(spacing: 15){
+                    Rectangle()
+                        .fill(Color("PrimaryColor"))
+                        .frame(height:1)
+                    
+                    Text("OR")
+                    
+                    Rectangle()
+                        .fill(Color("PrimaryColor"))
+                        .frame(height:1)
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 50)
+                
+                HStack(spacing: 25) {
+                    Button(action: {}){
+                        Image("google")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    }
+                    Button(action: {}){
+                        Image("fb")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    }
+                }
+                .padding(.top, 30)
+                
             }
+            .padding(.vertical)
+            .padding(.top, 60)
         }
         .background(Color("BackgroundColor").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
     }
@@ -68,16 +110,24 @@ struct LoginFormView: View {
     
     @State var email = ""
     @State var password = ""
-    
+    @Binding var index : Int
+
     var body: some View {
         
         ZStack(alignment: .bottom) {
             VStack{
                 HStack{
-                    Text("Login")
-                        .foregroundColor(.white)
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    VStack(spacing:10) {
+                        Text("Login")
+                            .foregroundColor(self.index == 0 ? .white : .gray)
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        
+                        Capsule()
+                            .fill(self.index == 0 ? Color.blue : Color.clear)
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 5)
+
+                    }
                     
                     Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
                 }
@@ -128,6 +178,12 @@ struct LoginFormView: View {
             .background(Color("SecondaryColor"))
             
             .clipShape(TriangleSlice())
+            .contentShape(TriangleSlice())
+            // Shadow
+            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5)
+            .onTapGesture {
+                self.index = 0
+            }
             .cornerRadius(35)
             .padding(.horizontal, 20)
             
@@ -143,6 +199,9 @@ struct LoginFormView: View {
                 
             }
             .offset(y:25)
+            // hides view when in background
+            // only button
+            .opacity(self.index == 0 ? 1 : 0)
         }
     }
 }
@@ -225,6 +284,11 @@ struct RegisterFormView: View {
             .clipShape(TriangleSliceRegister())
             // clipping the content shape for gesture
             .contentShape(TriangleSliceRegister())
+            // Shadow
+            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5)
+            .onTapGesture {
+                self.index = 1
+            }
             .cornerRadius(35)
             .padding(.horizontal, 20)
             
